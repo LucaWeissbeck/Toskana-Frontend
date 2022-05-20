7523f70a735d32c3b69145a4c0aa5428e9781c3a
Luca Weissbeck <Luca.Weissbeck@ibm.com>
2022-05-20 17:46:39 +0200

    Fixed design bug for video activity feed

--- src/app/user-pages/Login.js
+++ src/app/user-pages/Login.js
@@ -63,8 +63,6 @@ export class Login extends Component {
   render() {
     return (
       <div style={{ backgroundImage: `url(${backgroundImage2})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundAttachment: "fixed" }}>
-        {console.log("PROCESS ENV")}
-        {console.log(process.env.BACKEND_URL)}
         <div className="d-flex align-items-center auth px-0">
           <div className="row w-100 mx-0">
             <div className="col-lg-4 mx-auto">
